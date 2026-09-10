6cc01906405af2a6f613978151728aa569c92b7e
barjr <barjrwork@gmail.com>
2026-09-10 05:36:05 -0500

    Add portfolio landing page

---
+++ app/components/Header.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../public/anakin.jpg';
+import github from '../public/github.png';
+
+const Header = () => {
+  return (
+    <header className='flex justify-between items-center'>
+      <div className='logo'>
+        <Link href='/'>
+          <Image
+            src={logo}
+            loading='eager'
+            className='block w-10 h-10'
+            alt='Logo'
+          ></Image>
+        </Link>
+      </div>
+
+      <nav aria-label='Main Navigation' className='flex items-center'>
+        <ul className='flex items-center gap-3'>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#services'>Skills</a>
+          </li>
+          <li>
+            <a href='#contact'>Projects</a>
+          </li>
+        </ul>
+      </nav>
+      <div className='flex items-center gap-3'>
+        <a href='https://github.com/barjr'>
+          <Image
+            src={github}
+            className='block w-10 h-10'
+            loading='eager'
+            alt='Github Link'
+          ></Image>
+        </a>
+        <a href=''>Contact</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
